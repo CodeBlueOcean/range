dd33901c7860f0e713d7d428903048bd8d952f45
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-05 00:56:27 -0600

    Add files via upload

---
+++ range().py
@@ -0,0 +1,52 @@
+# print(range(100))
+
+print(range(0, 100))
+
+for number in range(0, 10):
+    print(number)
+
+for number in range(0, 10):
+    print('email email list')
+
+for _ in range(0, 10):
+    print(_)
+
+# another example:
+for _ in range(0, 10):
+    print(_)
+
+# another example:
+for _ in range(0, 10, 2):
+    print(_)
+
+# another example: not working because does make logical sense
+for _ in range(0, 10, -1):
+    print(_)
+
+# another example: showing
+for _ in range(10, 0, -1):
+    print(_)
+
+# another example: showing
+for _ in range(10, 0, -2):
+    print(_)
+
+# another example: won't work
+for _ in range(10, 0):
+    print(_)
+
+for _ in range(10, 0, -2):
+    print(list(range(10)))
+
+# another example: showing
+for _ in range(10, 2):
+    print(list(range(10)))
+
+# another example: give two range and 0-9
+for _ in range(2):
+    print(list(range(10)))
+
+ 
+
+    
+
